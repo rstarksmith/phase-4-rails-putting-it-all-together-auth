8416d161cf4dc477dabf9d2391a89d814ce66637
Rachel <rstark.smith@gmail.com>
2023-01-10 18:56:38 -0600

    commit tree b3320710107983519a3e6e6ab9acba30a2745147

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+    skip_before_action :authorize, only: :create
+
+
+    #POST /signup
+    def create 
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    #GET /me
+    def show
+        render json: @current_user, status: :created
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :image_url, :bio)
+    end
+end
